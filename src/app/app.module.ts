0f368a2463d9c60cfb36fedf914aa01284751cff
Robert Villarreal <robertv1979@gmail.com>
2017-08-14 13:27:31 -0700

    initial and final commit for Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
